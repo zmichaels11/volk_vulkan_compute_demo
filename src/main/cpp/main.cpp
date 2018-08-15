0c0c368baf896b454b778bb36e84655f3d577ead
zmichaels <zmichaels11@gmail.com>
2018-08-14 18:23:57 -0700

    Completed impementation

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -3,6 +3,7 @@
 #include <cstdint>
 
 #include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -16,6 +17,7 @@ struct context {
     VkPhysicalDevice physicalDevice;
     VkDevice device;
     VkPhysicalDeviceMemoryProperties memoryProperties;
+    std::vector<std::uint32_t> computeQueueFamilyIds;
 
     context();
 
@@ -32,13 +34,25 @@ void vkAssert(VkResult result);
 
 void bindMemory(context& ctx, VkBuffer buffer);
 
+std::vector<char> readFile(const std::string& fileName);
+
 int main(int argc, char** argv) {
     context ctx;
 
     auto inputData = std::vector<float>();
+    std::cout << "Compute Shader Squaring\n";
+    std::cout << "Inputs: [";
+
     for (int i = 0; i < 32; i++) {
-        inputData.push_back(static_cast<float> (2 << i));
+        inputData.push_back(static_cast<float> (i));
+        std::cout << inputData[i];
+
+        if (i != 31) {
+            std::cout << ", ";
+        }
     }
+
+    std::cout << "]" << std::endl;
 
     VkBufferCreateInfo bufferCI {};
     bufferCI.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
@@ -60,6 +74,195 @@ int main(int argc, char** argv) {
     vkAssert(vkCreateBuffer(ctx.device, &bufferCI, nullptr, &outputBuffer));
     auto outputMemory = ctx.bindMemory(outputBuffer);
 
+    auto descriptorSetLayoutBindings = std::vector<VkDescriptorSetLayoutBinding>();
+    {
+        VkDescriptorSetLayoutBinding binding {};
+        binding.binding = 0;
+        binding.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+        binding.descriptorCount = 1;
+        binding.stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+        
+        descriptorSetLayoutBindings.push_back(binding);
+
+        binding.binding = 1;
+        descriptorSetLayoutBindings.push_back(binding);
+    }
+
+    VkDescriptorSetLayoutCreateInfo descriptorSetLayoutCI {};
+    descriptorSetLayoutCI.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+    descriptorSetLayoutCI.bindingCount = descriptorSetLayoutBindings.size();
+    descriptorSetLayoutCI.pBindings = descriptorSetLayoutBindings.data();
+
+    VkDescriptorSetLayout descriptorSetLayout = VK_NULL_HANDLE;
+    vkAssert(vkCreateDescriptorSetLayout(ctx.device, &descriptorSetLayoutCI, nullptr, &descriptorSetLayout));
+
+    VkPipelineLayoutCreateInfo pipelineLayoutCI {};
+    pipelineLayoutCI.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    pipelineLayoutCI.setLayoutCount = 1;
+    pipelineLayoutCI.pSetLayouts = &descriptorSetLayout;
+
+    VkPipelineLayout pipelineLayout = VK_NULL_HANDLE;
+    vkAssert(vkCreatePipelineLayout(ctx.device, &pipelineLayoutCI, nullptr, &pipelineLayout));
+
+    auto descriptorSetPoolSizes = std::vector<VkDescriptorPoolSize>();
+    {
+        VkDescriptorPoolSize poolSize {};
+        poolSize.type = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+        poolSize.descriptorCount = 2;
+
+        descriptorSetPoolSizes.push_back(poolSize);
+    }
+
+    VkDescriptorPoolCreateInfo descriptorPoolCI {};
+    descriptorPoolCI.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+    descriptorPoolCI.maxSets = 2;
+    descriptorPoolCI.poolSizeCount = descriptorSetPoolSizes.size();
+    descriptorPoolCI.pPoolSizes = descriptorSetPoolSizes.data();
+
+    VkDescriptorPool descriptorPool = VK_NULL_HANDLE;
+    vkAssert(vkCreateDescriptorPool(ctx.device, &descriptorPoolCI, nullptr, &descriptorPool));
+
+    VkDescriptorSetAllocateInfo descriptorSetAI {};
+    descriptorSetAI.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+    descriptorSetAI.descriptorPool = descriptorPool;
+    descriptorSetAI.descriptorSetCount = 1;
+    descriptorSetAI.pSetLayouts = &descriptorSetLayout;
+
+    VkDescriptorSet descriptorSet = VK_NULL_HANDLE;
+    vkAssert(vkAllocateDescriptorSets(ctx.device, &descriptorSetAI, &descriptorSet));
+
+    auto descriptorBufferInfos = std::vector<VkDescriptorBufferInfo> (); 
+    {
+        VkDescriptorBufferInfo bufferInfo {};
+        bufferInfo.buffer = inputBuffer;
+        bufferInfo.offset = 0;
+        bufferInfo.range = VK_WHOLE_SIZE;
+
+        descriptorBufferInfos.push_back(bufferInfo);
+
+        bufferInfo.buffer = outputBuffer;
+        descriptorBufferInfos.push_back(bufferInfo);
+    }
+
+    auto descriptorSetWrites = std::vector<VkWriteDescriptorSet> ();
+    {
+
+        VkWriteDescriptorSet write {};
+        write.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+        write.descriptorCount = 1;
+        write.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+        write.pBufferInfo = descriptorBufferInfos.data();
+        write.dstBinding = 0;
+        write.dstSet = descriptorSet;
+
+        descriptorSetWrites.push_back(write);
+
+        write.pBufferInfo += 1;
+        write.dstBinding = 1;
+
+        descriptorSetWrites.push_back(write);
+    }
+
+    vkUpdateDescriptorSets(ctx.device, descriptorSetWrites.size(), descriptorSetWrites.data(), 0, nullptr);
+
+    auto spvCode = readFile("square.comp.spv");
+
+    VkShaderModuleCreateInfo shaderModuleCI {};
+    shaderModuleCI.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    shaderModuleCI.codeSize = spvCode.size();
+    shaderModuleCI.pCode = reinterpret_cast<const std::uint32_t *> (spvCode.data());
+
+    VkShaderModule computeShaderModule = VK_NULL_HANDLE;
+    vkAssert(vkCreateShaderModule(ctx.device, &shaderModuleCI, nullptr, &computeShaderModule));
+
+    VkPipelineShaderStageCreateInfo computeStageCI {};
+    computeStageCI.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    computeStageCI.stage = VK_SHADER_STAGE_COMPUTE_BIT;
+    computeStageCI.module = computeShaderModule;
+    computeStageCI.pName = "main";
+
+    VkComputePipelineCreateInfo computePipelineCI {};
+    computePipelineCI.sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO;
+    computePipelineCI.stage = computeStageCI;
+    computePipelineCI.layout = pipelineLayout;
+
+    VkPipeline pipeline = VK_NULL_HANDLE;
+    vkAssert(vkCreateComputePipelines(ctx.device, VK_NULL_HANDLE, 1, &computePipelineCI, nullptr, &pipeline));
+
+    vkDestroyShaderModule(ctx.device, computeShaderModule, nullptr);
+
+    VkCommandPoolCreateInfo commandPoolCI {};
+    commandPoolCI.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    commandPoolCI.queueFamilyIndex = ctx.computeQueueFamilyIds[0];
+    commandPoolCI.flags = VK_COMMAND_POOL_CREATE_TRANSIENT_BIT;
+
+    VkCommandPool commandPool = VK_NULL_HANDLE;
+    vkAssert(vkCreateCommandPool(ctx.device, &commandPoolCI, nullptr, &commandPool));
+
+    VkCommandBufferAllocateInfo commandBufferAI {};
+    commandBufferAI.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    commandBufferAI.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    commandBufferAI.commandPool = commandPool;
+    commandBufferAI.commandBufferCount = 1;
+
+    VkCommandBuffer commandBuffer = VK_NULL_HANDLE;
+    vkAssert(vkAllocateCommandBuffers(ctx.device, &commandBufferAI, &commandBuffer));
+
+    VkCommandBufferBeginInfo commandBufferBI {};
+    commandBufferBI.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+    commandBufferBI.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+    vkAssert(vkBeginCommandBuffer(commandBuffer, &commandBufferBI));
+
+    vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_COMPUTE, pipeline);
+    vkCmdBindDescriptorSets(commandBuffer, VK_PIPELINE_BIND_POINT_COMPUTE, pipelineLayout, 0, 1, &descriptorSet, 0, nullptr);
+    // local group size is (32x1x1); this command needs the number of groups.
+    vkCmdDispatch(commandBuffer, inputData.size() / 32, 1, 1);
+
+    vkAssert(vkEndCommandBuffer(commandBuffer));
+
+    VkQueue queue = VK_NULL_HANDLE;
+    vkGetDeviceQueue(ctx.device, ctx.computeQueueFamilyIds[0], 0, &queue);
+
+    VkFenceCreateInfo fenceCI {};
+    fenceCI.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+
+    VkFence taskCompleteFence = VK_NULL_HANDLE;
+    vkAssert(vkCreateFence(ctx.device, &fenceCI, nullptr, &taskCompleteFence));
+
+    VkSubmitInfo submitInfo {};
+    submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+    submitInfo.commandBufferCount = 1;
+    submitInfo.pCommandBuffers = &commandBuffer;
+    
+    vkAssert(vkQueueSubmit(queue, 1, &submitInfo, taskCompleteFence));
+    vkAssert(vkWaitForFences(ctx.device, 1, &taskCompleteFence, VK_TRUE, std::numeric_limits<std::uint64_t>::max()));
+
+    vkDestroyFence(ctx.device, taskCompleteFence, nullptr);
+    
+    float * pResults = nullptr;
+
+    vkAssert(vkMapMemory(ctx.device, outputMemory, 0, inputData.size() * sizeof(float), 0, reinterpret_cast<void **> (&pResults)));
+
+    std::cout << "Output: [";
+
+    for (int i = 0; i < inputData.size(); i++) {
+        std::cout << pResults[i];
+
+        if (i != inputData.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+
+    std::cout << "]" << std::endl;
+
+    vkUnmapMemory(ctx.device, outputMemory);
+
+    vkDestroyCommandPool(ctx.device, commandPool, nullptr);
+    vkDestroyPipeline(ctx.device, pipeline, nullptr);
+    vkDestroyDescriptorPool(ctx.device, descriptorPool, nullptr);
+    vkDestroyPipelineLayout(ctx.device, pipelineLayout, nullptr);
+    vkDestroyDescriptorSetLayout(ctx.device, descriptorSetLayout, nullptr);
     vkFreeMemory(ctx.device, outputMemory, nullptr);
     vkDestroyBuffer(ctx.device, outputBuffer, nullptr);
     vkFreeMemory(ctx.device, inputMemory, nullptr);
@@ -123,7 +326,7 @@ context::context() {
     auto familyProperties = std::make_unique<VkQueueFamilyProperties[]> (nQueueFamilies);
     vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &nQueueFamilies, familyProperties.get());
 
-    auto computeQueueFamilyIds = std::vector<std::uint32_t>();
+    computeQueueFamilyIds = std::vector<std::uint32_t>();
     for (std::uint32_t i = 0; i < nQueueFamilies; i++) {
         if (familyProperties[i].queueFlags & VK_QUEUE_COMPUTE_BIT) {
             computeQueueFamilyIds.push_back(i);
@@ -262,4 +465,23 @@ void vkAssert(VkResult result) {
     if (VK_SUCCESS != result) {
         throw std::runtime_error(translateVulkanResult(result));
     }
+}
+
+std::vector<char> readFile(const std::string& fileName) {
+    std::ifstream file(fileName.c_str(), std::ios::in | std::ios::binary | std::ios::ate);
+
+    if (file.is_open()) {
+        auto size = file.tellg();
+        auto out = std::vector<char>();
+        
+        out.resize(size);
+
+        file.seekg(0, std::ios::beg);
+        file.read(out.data(), size);
+        file.close();
+
+        return out;
+    }
+
+    throw std::runtime_error("Unable to open file: " + fileName);
 }
